3c7f4b178fb8deb8cbf693feb51428785b557c5f
Diogo Ferreira <defer@cyngn.com>
2016-04-03 11:08:35 +0100

    mt6735: Remove media_codecs_performance and media_profiles
    These are very device specific as performance varies depending on the
    thermal profile of the device and media profiles vary depending on the
    camera capabilities.
    
    Change-Id: Ib525c333e1c9ab00ccd2987b338afed176cbd2dc

--- mt6735.mk
+++ mt6735.mk
@@ -102,10 +102,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_codecs_mediatek_audio.xml:system/etc/media_codecs_mediatek_audio.xml \
     $(LOCAL_PATH)/configs/media_codecs_mediatek_video.xml:system/etc/media_codecs_mediatek_video.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/mtk_omx_core.cfg:system/etc/mtk_omx_core.cfg \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
